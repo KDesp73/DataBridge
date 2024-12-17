58def5ab7b03b4867e07e17d1737cbdd2c0c0d56
Konstantinos Despoinidis <kdesp2003@gmail.com>
2024-12-17 22:42:29 +0200

    updated docs

--- docs/db/d86/classio_1_1github_1_1kdesp73_1_1databridge_1_1helpers_1_1Config.js
+++ docs/db/d86/classio_1_1github_1_1kdesp73_1_1databridge_1_1helpers_1_1Config.js
@@ -1,6 +1,8 @@
 var classio_1_1github_1_1kdesp73_1_1databridge_1_1helpers_1_1Config =
 [
     [ "saveConfig", "db/d86/classio_1_1github_1_1kdesp73_1_1databridge_1_1helpers_1_1Config.html#accf4d03cea3102fb2a5e25bb00f80a90", null ],
+    [ "getLogLevel", "db/d86/classio_1_1github_1_1kdesp73_1_1databridge_1_1helpers_1_1Config.html#ac57c0bfec0894663b02e3fcf4943e41a", null ],
+    [ "getLogFile", "db/d86/classio_1_1github_1_1kdesp73_1_1databridge_1_1helpers_1_1Config.html#a1aa2fb4300c67f192b8fa594b18de921", null ],
     [ "getDbUrl", "db/d86/classio_1_1github_1_1kdesp73_1_1databridge_1_1helpers_1_1Config.html#afa9d9bef8aa47433273e64c077dfd702", null ],
     [ "getDbUser", "db/d86/classio_1_1github_1_1kdesp73_1_1databridge_1_1helpers_1_1Config.html#a96a80fefe22950f6abaa84649ff11bcb", null ],
     [ "getDbPassword", "db/d86/classio_1_1github_1_1kdesp73_1_1databridge_1_1helpers_1_1Config.html#a941994c827fea41ef30542ffbd79f765", null ],
